4e7c43f5b3f97591f1876f0ef6f84f68f800cf03
Daniel A. <1danielansias@gmail.com>
2025-01-06 19:55:32 +0100

    Style fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
       <a href="/"><h1 className="font-medium">
-        Transcrib<span className="text-blue-400 bold">elo</span>
+        Transcríb<span className="text-blue-400 bold">elo</span>
       </h1></a>
       <a href="/" className="flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400 text-sm">
         <p>Nuevo</p>
